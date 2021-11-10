cbfbe4e61ed1f8a47d08b03d6e6fbd59937c9a4b
Chris Boyke <chris.boyke@commercetools.com>
2021-11-10 15:38:56 -0800

    Added update type (add/remove field)

--- type.js
+++ type.js
@@ -3,15 +3,59 @@
 const ct = require('./commercetools');
 
 async function getType(key) {
-    console.log('Getting type for',key);
+  console.log('Getting type for',key);
+  return await ct.execute({
+    uri: ct.requestBuilder.types.byKey(key).build(),
+    method: 'GET',
+    allow404: true
+  });
+}
+
+// Supports add & remove of field definitions ONLY (for now)
+async function updateType(oldType,newType) {
+  const actions = [];  
+  for(let newFD of newType.fieldDefinitions) {
+    if(!oldType.fieldDefinitions.find(fd => fd.name == newFD.name)) {
+      // Add field defn
+      actions.push({
+        action: 'addFieldDefinition',
+        fieldDefinition: newFD
+      });
+    }
+  }
+  for(let oldFD of oldType.fieldDefinitions) {
+    if(!newType.fieldDefinitions.find(fd => fd.name == oldFD.name)) {
+      // Remove field defn
+      actions.push({
+        action: 'removeFieldDefinition',
+        fieldName: oldFD.name
+      });
+    }
+  }
+  if(actions.length==0) {
+    console.log('no changes');
+  } else {
+    console.log(actions);
+    console.log('Update type for',newType.key);
+    
     return await ct.execute({
-      uri: ct.requestBuilder.types.byKey(key).build(),
-      method: 'GET',
-      allow404: true
+      uri: ct.requestBuilder.types.byKey(newType.key).build(),
+      method: 'POST',
+      body: {
+        version: oldType.version,
+        actions: actions
+      }
     });
+    
   }
 
-async function createType(body) {
+}
+
+async function createOrUpdateType(body) {
+  const existing = await getType(body.key);
+  if(existing) {
+    updateType(existing.body,body);
+  } else {
     console.log('Create type for',body.key);
     return await ct.execute({
       uri: ct.requestBuilder.types.build(),
@@ -19,17 +63,18 @@ async function createType(body) {
       body: body
     });
   }
+}
   
-  async function deleteType(key,version) {
-    console.log('Deleting type',key,'version',version);
-    return await ct.execute({
-      uri: ct.requestBuilder.types.byKey(key).build() + '?version='+version,
-      method: 'DELETE',
-    });
-  }
+async function deleteType(key,version) {
+  console.log('Deleting type',key,'version',version);
+  return await ct.execute({
+    uri: ct.requestBuilder.types.byKey(key).build() + '?version='+version,
+    method: 'DELETE',
+  });
+}
 
-  module.exports = {
-      getType,
-      createType,
-      deleteType
-  }
+module.exports = {
+    getType,
+    createOrUpdateType,
+    deleteType
+}
